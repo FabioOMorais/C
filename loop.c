f47ae8e3c11d6a63364dc18037928d3aa420bb0e
FábioMorais <86443864+FabioOMorais@users.noreply.github.com>
2021-09-10 10:53:09 -0300

    Add files via upload

---
+++ loop.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int contador;
+    int numero = 1;
+
+    for(contador <= 10; contador + 1;){
+
+        printf("8x%i\n", contador);
+    }
+
+
+
+
+
+
+
+
+    return EXIT_SUCCESS;
+}
