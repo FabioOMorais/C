17fba512fc2f67821960b3c5da8d279c8d49e560
FábioMorais <86443864+FabioOMorais@users.noreply.github.com>
2021-09-12 00:23:48 -0300

    Add files via upload

---
+++ tabuada.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int contador;
+    int n;
+
+    printf("Escolha um numero da tabuada: \n");
+    scanf("%i", &n);
+
+    for(contador = 1; contador <= 10; contador++){
+
+
+        printf("%ix%i=%i\n", n, contador, contador*n);
+    }
+
+
+    return EXIT_SUCCESS;
+}
