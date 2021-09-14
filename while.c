e91e1df8e67661e3b5dc21f5435ceb86f191c42a
FábioMorais <86443864+FabioOMorais@users.noreply.github.com>
2021-09-14 11:46:08 -0300

    Add files via upload

---
+++ while.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+
+    int contador;
+    
+    while(contador <= 10){
+
+        printf("%i\n", contador);
+        ++contador;
+    }
+
+return EXIT_SUCCESS;
+}
